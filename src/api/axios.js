54284d25e8985bfdd1cc0d23e9219f5c706f549d
Henry <Henries90@gmail.com>
2025-07-20 00:14:44 +0100

    Removed localhost

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  //  baseURL: "https://dsdhj-api.onrender.com/",
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://dsdhj-api.onrender.com/",
+  // baseURL: "http://localhost:5000/",
   withCredentials: true,
 });
