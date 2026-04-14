e3dab8d9b3d24431aaf1b86073a22b7962a3eb91
Henry <Henries90@gmail.com>
2026-04-14 17:14:19 +0100

    Removed localhost from prod

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  // baseURL: "https://api.dsdhj.ng/",
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://api.dsdhj.ng/",
+  // baseURL: "http://localhost:5000/",
   withCredentials: true,
 });
